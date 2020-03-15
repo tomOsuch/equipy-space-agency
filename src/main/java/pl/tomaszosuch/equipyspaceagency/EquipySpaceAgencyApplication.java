6350384efb9af0f3b4620adfd6039858e74efe51
tomOsuch <tomasz.osuch@gmail.com>
2020-03-16 00:53:15 +0100

    Initial commit

---
+++ src/main/java/pl/tomaszosuch/equipyspaceagency/EquipySpaceAgencyApplication.java
@@ -0,0 +1,13 @@
+package pl.tomaszosuch.equipyspaceagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipySpaceAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipySpaceAgencyApplication.class, args);
+    }
+
+}
